e1a10ca7e3495b511a9e2e3b75e12f45a666f4cb
Sofia Silva <sofia@amie.so>
2023-06-20 02:03:27 +0100

    add `pathPrefix` to config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,6 +9,7 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
+  pathPrefix: "/portocampustudios",
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-transformer-remark`,
